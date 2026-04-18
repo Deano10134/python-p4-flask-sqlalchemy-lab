22862d4246c81507cfd7047e8ed7f1060d5ea57d
Dean Baberowski <deanbaberows@gmail.com>
2026-04-18 15:21:12 +1000

    complete lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,35 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get_or_404(id)
+    zookeeper_name = animal.zookeeper.name if animal.zookeeper else 'None'
+    enclosure_env = animal.enclosure.environment if animal.enclosure else 'None'
+    return f'''
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {zookeeper_name}</ul>
+        <ul>Enclosure: {enclosure_env}</ul>
+    '''
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get_or_404(id)
+    animals_html = ''.join(f'<ul>Animal: {a.name}</ul>' for a in zookeeper.animals)
+    return f'''
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+        {animals_html}
+    '''
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get_or_404(id)
+    animals_html = ''.join(f'<ul>Animal: {a.name}</ul>' for a in enclosure.animals)
+    return f'''
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+        {animals_html}
+    '''
 
 
 if __name__ == '__main__':
